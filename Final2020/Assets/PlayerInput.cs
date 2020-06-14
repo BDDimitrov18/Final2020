cc94254d2ab96a2276a3cbdbe67ed939999560d8
BDDimitrov18 <58163537+BDDimitrov18@users.noreply.github.com>
2020-06-14 21:01:39 +0300

    PlayerMovement

---
+++ Final2020/Assets/PlayerInput.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    private float x, y; 
+    public float Speed = 3f;
+    public PlayerMovement mov;
+    private void Update()
+    {
+        x = Input.GetAxisRaw("Horizontal") * Speed;
+        y = Input.GetAxisRaw("Vertical") * Speed;
+    }
+
+    private void FixedUpdate()
+    {
+        mov.Move(x, y);
+    }
+}
