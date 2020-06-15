eeae88b00593571a04b6bc30ebdefb56592a9483
BDDimitrov18 <58163537+BDDimitrov18@users.noreply.github.com>
2020-06-16 01:15:43 +0300

    Pick Crop Menu Update
    Add on hover effect.
    The slots now know if tey are selected or not and can execute operations.

---
+++ Final2020/Assets/Scripts/ItemSlotScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSlotScript : MonoBehaviour
+{
+    public bool isHovered;
+    Animator anim;
+
+    private void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        anim.SetBool("Hovered", isHovered);
+        if (isHovered)
+        {
+            if (Input.GetKeyDown("e"))
+            { 
+                //sthHappens
+            }
+        }
+    }
+}
---
+++ Final2020/Assets/Scripts/OnHoverUi.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnHoverUi : MonoBehaviour
+{
+    public SelectorProperties selectorScript;
+    public List<GameObject> itemSlots;
+    public int selectedID=0;
+    int lastSelectedId =0;
+
+    private void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.gameObject.tag == "ItemSlot")
+            {
+                itemSlots.Add(child.gameObject);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("right"))
+       {
+            selectedID++;
+        }
+        if (Input.GetKeyDown("left"))
+        {
+            selectedID--;
+        }
+        if (Input.GetKeyDown("up"))
+        {
+            selectedID -= 3;
+        }
+        if (Input.GetKeyDown("down"))
+        {
+            selectedID += 3;
+        }
+
+        if (selectedID < 0)
+        {
+            selectedID = 0;
+        }
+        if (selectedID > itemSlots.Count)
+        {
+            selectedID = itemSlots.Count;
+        }
+        if (lastSelectedId != selectedID)
+        {
+            itemSlots[lastSelectedId].GetComponent<ItemSlotScript>().isHovered = false;
+        }
+        itemSlots[selectedID].GetComponent<ItemSlotScript>().isHovered = true;
+        lastSelectedId = selectedID;
+    }
+}
