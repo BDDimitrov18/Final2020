a5127f0f542a92f48e40e213e8236599600190b6
kamilanov18 <54852657+kamilanov18@users.noreply.github.com>
2020-06-26 01:27:35 +0300

    Add experimental download buttons

--- Website/JavaScript/index.js
+++ Website/JavaScript/index.js
@@ -1,8 +1,7 @@
-
 var i = 0;
-var sentences = ["Farming","Money","Economy","Rivalry"];
+var sentences = ["Farming", "Money", "Economy", "Rivalry"];
 var txt = 'Lorem ipsum dummy text blabla. DADADADADADASDAD';
-var sentenceIndex=0;
+var sentenceIndex = 0;
 var k = txt.length;
 var speed = 100;
 
@@ -12,36 +11,33 @@ function typeWriter() {
         document.getElementById("demo").textContent += sentences[sentenceIndex].charAt(i);
         i++;
         setTimeout(typeWriter, speed);
-    }
-    else
-        setTimeout(deleteText,speed);
+    } else
+        setTimeout(deleteText, speed);
 }
 
 async function deleteText() {
     if (i > 0) {
-        document.getElementById("demo").textContent = sentences[sentenceIndex].substr(0, i-1);
+        document.getElementById("demo").textContent = sentences[sentenceIndex].substr(0, i - 1);
         console.log(txt.substr(0, i));
         i--;
         setTimeout(deleteText, speed);
-    }
-    else
-    {
+    } else {
         sentenceIndex++;
-        if(sentenceIndex>=sentences.length) sentenceIndex=0;
-        setTimeout(typeWriter(),500);
+        if (sentenceIndex >= sentences.length) sentenceIndex = 0;
+        setTimeout(typeWriter(), 500);
     }
 }
 
-function lightMode()
-{
-    $("body").css("background-color","white");
-    $(".HRstyle").css("border-color","rgb(56, 56, 56)");
-    $("body").css("color","black");
+function lightMode() {
+    $("body").css("background-color", "white");
+    $(".HRstyle").css("border-color", "rgb(56, 56, 56)");
+    $("body").css("color", "black");
 }
 
-function darkMode()
-{
-    $("body").css("background-color","#2f3640");
-    $(".HRstyle").css("border-color","white");
-    $("body").css("color","white");
+function darkMode() {
+    $("body").css("background-color", "#2f3640");
+    $(".HRstyle").css("border-color", "white");
+    $("body").css("color", "white");
+    $("footer").css("background-color", "rgb(46, 46, 46)");
+    $("header").css("background-color", "rgb(46, 46, 46)")
 }
