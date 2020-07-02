e8e9ba0e06497518bb9223a261c74f5fb76716ad
BDDimitrov18 <58163537+BDDimitrov18@users.noreply.github.com>
2020-07-02 05:43:12 +0300

    Final Changes To The Game
    *published zip for the game
    *made a video
    *exported 2 versions
    *fixed bugs

---
+++ Website/JavaScript/index.js
@@ -0,0 +1,43 @@
+var i = 0;
+var sentences = ["Farming", "Money", "Economy", "Rivalry"];
+var txt = 'Lorem ipsum dummy text blabla. DADADADADADASDAD';
+var sentenceIndex = 0;
+var k = txt.length;
+var speed = 100;
+
+function typeWriter() {
+
+    if (i < txt.length) {
+        document.getElementById("demo").textContent += sentences[sentenceIndex].charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    } else
+        setTimeout(deleteText, speed);
+}
+
+async function deleteText() {
+    if (i > 0) {
+        document.getElementById("demo").textContent = sentences[sentenceIndex].substr(0, i - 1);
+        console.log(txt.substr(0, i));
+        i--;
+        setTimeout(deleteText, speed);
+    } else {
+        sentenceIndex++;
+        if (sentenceIndex >= sentences.length) sentenceIndex = 0;
+        setTimeout(typeWriter(), 500);
+    }
+}
+
+function lightMode() {
+    $("body").css("background-color", "white");
+    $(".HRstyle").css("border-color", "rgb(56, 56, 56)");
+    $("body").css("color", "black");
+}
+
+function darkMode() {
+    $("body").css("background-color", "#2f3640");
+    $(".HRstyle").css("border-color", "white");
+    $("body").css("color", "white");
+    $("footer").css("background-color", "rgb(46, 46, 46)");
+    $("header").css("background-color", "rgb(46, 46, 46)")
+}
